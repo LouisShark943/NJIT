905d184d54df1eebdef4bb13c3419813b6b1c616
LouisShark943 <36343680+LouisShark943@users.noreply.github.com>
2018-02-10 16:39:43 -0500

    Add files via upload

---
+++ My script.py
@@ -0,0 +1,15 @@
+#This is my fault
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+num3 = num1 + num2
+
+#print("The result of num1 plus num2 = num3")
+
+print("c) The result of num1 plus num2 = " + str(num3) )
+
+end = input("")
---
+++ Square root script.py
@@ -0,0 +1,8 @@
+import math
+
+while True:
+    number = int(input("Enter number to find squareroot: "))
+    result = math.sqrt(number)
+    print("The square root of " + str(number) + "is: " + str(result))
+
+end = input("")
